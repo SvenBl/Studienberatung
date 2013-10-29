f014b3e3960ac2acf2e65899302c06e0a707709d
SvenBl <sven.blanck@haw-hamburg.de>
2013-10-29 22:57:59 +0100

    added Wissbegierigfrage

---
+++ Studienberatung.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Studienberatung(){
+	public static void main(String[]args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bist du wissbegierig?");
+		String answer = scanner.next();
+		if(answer.equalsIgnoreCase("n")){
+			
+		}
+		else{}
+		
+	}
+}
