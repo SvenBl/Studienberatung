0d4c470f48472b7d3ba4b82146bf5522e8052e3e
SvenBl <sven.blanck@haw-hamburg.de>
2013-10-29 23:08:13 +0100

    added berufsausbildung

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,23 +3,17 @@ import java.util.*;
 public class Studienberatung(){
 	public static void main(String[]args){
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Hast du Abitur?");
+		System.out.println("Hast du eine Berufsausbildung?");
 		String answer = scanner.next();
 		
 		if(answer.equalsIgnoreCase("n")){
-			System.out.println("Hast du Fachabitur?");
-			String answer = scanner.next();
-			if(answer.equalsIgnoreCase("n")){}
-			else{
+
 			}
 			
 			
 		}
 		else{
-			System.out.println("Bist du wissbegierig?");
-			String answer = scanner.next();
-			if(answer.equalsIgnoreCase("n")){}
-			else{
+
 			}
 		}
 		
