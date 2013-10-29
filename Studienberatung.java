7778f7f8b1c2a37de9a4cba7ace8f9b2064a604a
SvenBl <sven.blanck@haw-hamburg.de>
2013-10-29 23:15:28 +0100

    merged all

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,17 +3,29 @@ import java.util.*;
 public class Studienberatung(){
 	public static void main(String[]args){
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Hast du eine Berufsausbildung?");
+		System.out.println("Hast du Abitur?");
 		String answer = scanner.next();
 		
 		if(answer.equalsIgnoreCase("n")){
-
+			System.out.println("Hast du Fachabitur?");
+			String answer = scanner.next();
+			if(answer.equalsIgnoreCase("n")){
+				System.out.println("Hast du eine Berufsausbildung?");
+				String answer = scanner.next();
+				if(answer.equalsIgnoreCase("n")){}
+				else{
+				}
+			}
+			else{
 			}
 			
 			
 		}
 		else{
-
+			System.out.println("Bist du wissbegierig?");
+			String answer = scanner.next();
+			if(answer.equalsIgnoreCase("n")){}
+			else{
 			}
 		}
 		
